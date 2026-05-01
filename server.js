3cee12c98fb3b554be93ec7a54c315260f140e95
vivoivp-create <vivoivp@gmail.com>
2026-05-01 14:24:48 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const path = require('path');
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
